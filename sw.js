2a76d31e327c6b08483236bff345f17f5f96ce89
vvlietsam-lab <vvlietsam@gmail.com>
2026-08-19 17:19:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kantine-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'kantine-v4';
+const ASSETS = ['./', './index.html', './content.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
